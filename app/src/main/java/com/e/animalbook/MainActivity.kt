a6c3b80d3496914161385d08cf1dc5a7f2b3863f
kghghth <n8s6b4C9@iclud.com>
2020-08-23 17:34:27 +0900

    develop

--- app/src/main/java/com/e/animalbook/MainActivity.kt
+++ app/src/main/java/com/e/animalbook/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.e.animalbook
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
--- app/src/main/java/com/e/animalbook/SubActivity.kt
+++ app/src/main/java/com/e/animalbook/SubActivity.kt
@@ -3,13 +3,14 @@ package com.e.animalbook
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_sub.*
 
 class SubActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
 
-        lionButton.setOnClickListener {
+        lionbutton.setOnClickListener {
             val fragment = LionFragment()
             val fragmentManager = this.getSupportFragmentManager()
             val fragmentTransaction = fragmentManager.beginTransaction()
@@ -19,7 +20,7 @@ class SubActivity : AppCompatActivity() {
 
         }
 
-        hippoButton.setOnClickListener {
+        hippobutton.setOnClickListener {
             val fragment = HippoFragment()
             val fragmentManager = this.getSupportFragmentManager()
             val fragmentTransaction = fragmentManager.beginTransaction()
