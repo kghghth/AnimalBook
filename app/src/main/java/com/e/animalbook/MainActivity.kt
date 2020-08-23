34039c2d05c7e2d7437937b4613b6629da99e355
kghghth <n8s6b4C9@iclud.com>
2020-08-23 14:08:32 +0900

    develop

---
+++ app/src/main/java/com/e/animalbook/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.e.animalbook
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val fragment = titleFragment as? TitleFragment
+        fragment?.setTitle("フラグメント動物図鑑")
+
+        nextButton.setOnClickListener {
+            val intent = Intent (this,SubActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
